dff0dbb8d0bf92f7c1edcba2b9693c5f9fc01341
Eric Wieser <wieser.eric@gmail.com>
2020-07-21 13:57:06 +0100

    fixup: Relax some more constraints to try and make the build succeed

--- src/linear_algebra/direct_sum_module.lean
+++ src/linear_algebra/direct_sum_module.lean
@@ -35,7 +35,7 @@ namespace direct_sum
 
 variables {R ι M}
 
-instance : semimodule R (direct_sum ι M) := @dfinsupp.to_semimodule ι M _ _ (λ i, by apply_instance) _
+instance : semimodule R (direct_sum ι M) := @dfinsupp.to_semimodule ι M _ _ _ _
 
 variables R ι M
 /-- Create the direct sum given a family `M` of `R` semimodules indexed over `ι`. -/
