5fa40201f9d42baf1d18f3b42d047ff7160147ad
Floris van Doorn <fpvdoorn@gmail.com>
2020-07-21 14:27:23 -0400

    stop tracing

--- src/data/equiv/local_equiv.lean
+++ src/data/equiv/local_equiv.lean
@@ -79,7 +79,7 @@ set.inter_subset_left set.mem_prod set.range_id and_self set.mem_range_self
 eq_self_iff_true forall_const forall_true_iff set.inter_univ set.preimage_id function.comp.right_id
 not_false_iff and_imp set.prod_inter_prod set.univ_prod_univ true_or or_true
 
-set_option trace.simps.verbose true
+-- set_option trace.simps.verbose true
 
 namespace tactic.interactive
 
