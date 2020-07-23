d34ce53209f64721ce1e82b9e35c7aa8b7faffad
Pim <spelier.pim@gmail.com>
2020-07-23 13:48:29 +0200

    Changed definition of problem

--- src/computability/problem.lean
+++ src/computability/problem.lean
@@ -5,28 +5,28 @@ open function
 
 namespace problem
 
-structure problem (α : Type*) extends primcodable α :=
+structure problem (α : Type*) [primcodable α] :=
 (yesinstance : α → Prop)
 
-instance : primcodable ℕ := { 
+instance : primcodable ℕ := {
   encode := id,
   decode := some,
   encodek := by simp,
-  prim := nat.primrec.succ 
-} 
+  prim := nat.primrec.succ
+}
 
-def is_even [h : primcodable ℕ]: problem ℕ := { 
+def is_even [h : primcodable ℕ]: problem ℕ := {
   yesinstance := λ n, n%2 = 0,
   ..h,
 }
 
-def is_odd [h : primcodable ℕ]: problem ℕ := { 
+def is_odd [h : primcodable ℕ]: problem ℕ := {
   yesinstance := λ n, n%2 = 1,
   ..h,
 }
 
-def many_one_reducible {α β} (P : problem α) (Q : problem β) :=
-∃ f : α → β, @computable α β P.to_primcodable Q.to_primcodable f ∧ ∀ a, P.yesinstance a ↔ Q.yesinstance (f a)
+def many_one_reducible {α β} [primcodable α] [primcodable β] (P : problem α) (Q : problem β) :=
+∃ f : α → β, computable f ∧ ∀ a, P.yesinstance a ↔ Q.yesinstance (f a)
 -- ∃ f : α → β, computable f ∧ ∀ a, p a ↔ q (f a)
 
 infix ` ≤₀ `:1000 := many_one_reducible
