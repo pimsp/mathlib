3ab5202e3d1f7aceeb7b6b2920db1cbd63713283
MadPidgeon <daanvangent@gmail.com>
2020-07-23 12:38:05 +0200

    is_even_red_to_is_odd in progress

--- src/computability/problem.lean
+++ src/computability/problem.lean
@@ -2,6 +2,8 @@ import computability.primrec
 import computability.reduce
 open function
 
+namespace problem
+
 structure problem (α : Type*) extends primcodable α :=
 (yesinstance : α → Prop)
 
@@ -13,17 +15,32 @@ instance : primcodable ℕ := {
 } 
 
 def is_even [h : primcodable ℕ]: problem ℕ := { 
-  yesinstance := λ n, n%2 = 1,
-  ..h,
-}
-
-
-def is_odd [h : primcodable ℕ]: problem ℕ := { 
   yesinstance := λ n, n%2 = 0,
   ..h,
 }
 
-def many_one_reducible {α β} (P : problem α) (Q : problem β) (p : α → Prop) (q : β → Prop) :=
-∃ f, computable f ∧ ∀ a, p a ↔ q (f a)
+def is_odd [h : primcodable ℕ]: problem ℕ := { 
+  yesinstance := λ n, n%2 = 1,
+  ..h,
+}
+
+def many_one_reducible {α β} (P : problem α) (Q : problem β) :=
+∃ f : α → β, @computable α β P.to_primcodable Q.to_primcodable f ∧ ∀ a, P.yesinstance a ↔ Q.yesinstance (f a)
+-- ∃ f : α → β, computable f ∧ ∀ a, p a ↔ q (f a)
 
 infix ` ≤₀ `:1000 := many_one_reducible
+
+lemma is_even_red_to_is_odd: is_even ≤₀ is_odd := begin
+  use nat.succ,
+  split,
+  exact computable.succ,
+  intro a,
+  split, {
+    intro ah,
+    change a.succ % 2 = 1,
+    have ah' : a % 2 = 0, exact ah,
+  }
+
+end
+
+end problem
