c838a81d8aa993f1f7a8f777ea8d63c51e8c44a6
MadPidgeon <daanvangent@gmail.com>
2020-07-23 13:46:20 +0200

    fixing even_add_odd...

--- src/computability/problem.lean
+++ src/computability/problem.lean
@@ -1,3 +1,4 @@
+import tactic
 import computability.primrec
 import computability.reduce
 open function
@@ -30,17 +31,43 @@ def many_one_reducible {α β} (P : problem α) (Q : problem β) :=
 
 infix ` ≤₀ `:1000 := many_one_reducible
 
+lemma even_add_odd_is_odd (a:ℤ) (b:ℤ): b%2=1 → ( a%2=0 ↔ (a+b)%2=1 ) := begin
+  intro bh,
+  split, {
+    intro ah,
+    calc (a+b) % 2 = (a%2+b%2)%2 : by simp
+    ... = (0+1)%2 : by rw [ah,bh]
+    ... = 1 : by ring,
+  },
+  intro ah,
+  calc a % 2 = (a+2)%2 : by simp
+  ... = (a+1+1)%2 : by ring
+  ... = ((a+1)%2 + 1%2)%2 : by simp
+  ... = (1+1%2)%2 : by rw ah
+  ... = 0 : by ring,
+end
+
 lemma is_even_red_to_is_odd: is_even ≤₀ is_odd := begin
   use nat.succ,
   split,
   exact computable.succ,
   intro a,
+  have h1 :(1:ℤ)%2=1 := by ring,
+  have h2 :↑(2:ℕ) = (2:ℤ) := by simp,
+  change a%2 = 0 ↔ (a+1) % 2 = 1,
   split, {
     intro ah,
-    change a.succ % 2 = 1,
-    have ah' : a % 2 = 0, exact ah,
+    apply int.coe_nat_inj,
+    apply (even_add_odd_is_odd a 1 h1).mp,
+    rw ← h2,
+    rw ← int.coe_nat_mod,
+    rw ah,
+    ring,
+  }, {
+    intro ah,
+    apply int.coe_nat_inj,
+    dsimp,
   }
-
 end
 
 end problem
