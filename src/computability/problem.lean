c6b2a5ded6f266d2d33da2fffd1d22c5fafae238
MadPidgeon <daanvangent@gmail.com>
2020-07-23 11:41:18 +0200

    primcodable def \N

--- src/computability/problem.lean
+++ src/computability/problem.lean
@@ -1,25 +1,27 @@
 import computability.primrec
+import computability.reduce
+open function
 
 structure problem (α : Type*) extends primcodable α :=
 (yesinstance : α → Prop)
 
--- instance : problem ℕ is_even :=
-
-example (α : Type) some α:
-
-def is_even : problem ℕ :=
-{ encode := id,
+instance : primcodable ℕ := { 
+  encode := id,
   decode := some,
-  encodek := by simp only [forall_const, id.def, eq_self_iff_true],
-  prim := nat.primrec.succ,
-  yesinstance := λ n, n%2 = 0}
+  encodek := by simp,
+  prim := nat.primrec.succ 
+} 
 
-def is_odd : problem ℕ :=
-{ encode := id,
-  decode := some,
-  encodek := by simp only [forall_const, id.def, eq_self_iff_true],
-  prim := nat.primrec.succ,
-  yesinstance := λ n, n%2 = 1}
+def is_even [h : primcodable ℕ]: problem ℕ := { 
+  yesinstance := λ n, n%2 = 1,
+  ..h,
+}
+
+
+def is_odd [h : primcodable ℕ]: problem ℕ := { 
+  yesinstance := λ n, n%2 = 0,
+  ..h,
+}
 
 def many_one_reducible {α β} (P : problem α) (Q : problem β) (p : α → Prop) (q : β → Prop) :=
 ∃ f, computable f ∧ ∀ a, p a ↔ q (f a)
