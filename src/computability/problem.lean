0dd6ad3ebffbafbd373cbac9a6084e90e2be8974
Pim <spelier.pim@gmail.com>
2020-07-23 11:30:03 +0200

    Finished defining is_even and added is_odd

--- src/computability/problem.lean
+++ src/computability/problem.lean
@@ -1,20 +1,27 @@
 import computability.primrec
 
 structure problem (α : Type*) extends primcodable α :=
-(yesness : α → Prop)
+(yesinstance : α → Prop)
 
 -- instance : problem ℕ is_even :=
 
-example (α : Type) : α → option α :=
-begin
-  exact some,
-end
+example (α : Type) some α:
 
 def is_even : problem ℕ :=
 { encode := id,
   decode := some,
-  encodek := by simp,
-  prim := begin
-    sorry,
-  end,
-  yesness := λ n, n%2 = 0}
+  encodek := by simp only [forall_const, id.def, eq_self_iff_true],
+  prim := nat.primrec.succ,
+  yesinstance := λ n, n%2 = 0}
+
+def is_odd : problem ℕ :=
+{ encode := id,
+  decode := some,
+  encodek := by simp only [forall_const, id.def, eq_self_iff_true],
+  prim := nat.primrec.succ,
+  yesinstance := λ n, n%2 = 1}
+
+def many_one_reducible {α β} (P : problem α) (Q : problem β) (p : α → Prop) (q : β → Prop) :=
+∃ f, computable f ∧ ∀ a, p a ↔ q (f a)
+
+infix ` ≤₀ `:1000 := many_one_reducible
